741f611d60b9476dd630cf305b2cdb3f761ab62b
Gaspar <e972136@gmail.com>
2022-07-22 11:07:32 -0600

    Primero

---
+++ SpringBootJDBFlywayDocker/src/main/java/com/gaspar/ws/SpringBootJdbFlywayDockerApplication.java
@@ -0,0 +1,13 @@
+package com.gaspar.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbFlywayDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbFlywayDockerApplication.class, args);
+	}
+
+}
